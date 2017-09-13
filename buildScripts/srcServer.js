9ebe7ac996268c531237411e27c52788c1bdfaeb
jonathandewe <31571599+jonathandewe@users.noreply.github.com>
2017-09-13 23:01:26 +0100

    Commit

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,25 +1,36 @@
-var express = require('express');
-path = require('path');
-open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port =3000;
-var app =express();
+/*eslint no-console: */
 
-app.get('/', function(req,res){
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
 
-res.sendFile(path.join(__dirname, '../src/index.html'));
-});
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+  });
 
 app.listen(port, function(err)
 
 {
 if(err){
 
-console.log(err);}
+console.log(err); 
+
+}
 else {
 
 open('http://localhost:'+ port);
 }
 }
 
-);
+); 
