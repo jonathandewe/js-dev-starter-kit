770f5ad7f5c6b0e78a96b43e17116226e781afa1
jonathandewe <31571599+jonathandewe@users.noreply.github.com>
2017-09-16 22:35:45 +0100

    Update

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,5 @@
 import path from 'path';
-
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,7 +15,26 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
-
+// Create HTML file that includes reference to bundled JS.
+new HtmlWebpackPlugin({
+  template: 'src/index.html',
+  // minify: {
+  //   removeComments: true,
+  //   collapseWhitespace: true,
+  //   removeRedundantAttributes: true,
+  //   useShortDoctype: true,
+  //   removeEmptyAttributes: true,
+  //   removeStyleLinkTypeAttributes: true,
+  //   keepClosingSlash: true,
+  //   minifyJS: true,
+  //   minifyCSS: true,
+  //   minifyURLs: true
+  // },
+  inject: true,
+  // Properties you define here are available in index.html
+  // using htmlWebpackPlugin.options.varName
+  // trackJSToken: '43ad216f57d94259968435894490a5c7'
+}),
   ],
   module: {
     loaders: [
